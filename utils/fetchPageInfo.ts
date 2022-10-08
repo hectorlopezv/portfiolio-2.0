a091759f27092f90dcd481972ad49694c44abcf6
hvlopez <hectorvmlopez@gmail.com>
2022-10-07 20:42:37 -0500

    feat(EPMEDU-764): fxing styles for cover pill

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -2,7 +2,7 @@ import { PageInfo } from "../typings";
 
 export const FetchPageInfo = async () => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`
+    `https://portfiolio4.vercel.app/api/getPageInfo`
   );
   const data = await res?.json();
   const pageInfo: PageInfo = data?.pageInfo;
--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -1,9 +1,7 @@
 import { Project } from "../typings";
 
 export const FetchProjects = async () => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`
-  );
+  const res = await fetch(`https://portfiolio4.vercel.app/api/getProjects`);
   const data = await res?.json();
   const projects: Project[] = data?.projects;
   return projects;
